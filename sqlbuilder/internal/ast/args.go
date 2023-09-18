860da5225d216e59a260ffd924447f576d643eca
Connor Szczepaniak <cszczepaniak@gmail.com>
2023-09-17 19:03:53 -0500

    get rid of Visit

--- sqlbuilder/internal/ast/args.go
+++ sqlbuilder/internal/ast/args.go
@@ -1,13 +1,9 @@
 package ast
 
-func Visit(n Node, fn func(n Node) bool) {
-	n.AcceptVisitor(fn)
-}
-
 func GetArgs(n Node) []any {
 	var args []any
 
-	Visit(n, func(n Node) bool {
+	n.AcceptVisitor(func(n Node) bool {
 		ph, ok := n.(*PlaceholderLiteral)
 		if !ok {
 			// Keep traversing the tree
