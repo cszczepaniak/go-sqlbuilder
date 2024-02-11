5617d96530189007dc2ff5adad3f644e19875ba2
Connor Szczepaniak <cszczepaniak@gmail.com>
2024-02-11 16:46:23 -0600

    remove SelectTarget

--- sqlbuilder/internal/sel/target.go
+++ sqlbuilder/internal/sel/target.go
@@ -3,7 +3,6 @@ package sel
 import "github.com/cszczepaniak/go-sqlbuilder/sqlbuilder/internal/ast"
 
 type Target interface {
-	SelectTarget() (string, error)
 	ast.IntoTableExpr
 }
 
@@ -11,8 +10,4 @@ type Table string
 
 func (t Table) IntoTableExpr() ast.TableExpr {
 	return ast.NewTableName(string(t))
-}
-
-func (t Table) SelectTarget() (string, error) {
-	return string(t), nil
 }
