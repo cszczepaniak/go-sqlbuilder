830c0584329a3f58d3e1731d0e0faafe09aac2eb
Connor Szczepaniak <cszczepaniak@gmail.com>
2024-03-02 11:47:32 -0600

    add alter builder

--- sqlbuilder/internal/table/interfaces.go
+++ sqlbuilder/internal/table/interfaces.go
@@ -10,6 +10,10 @@ type columnBuilder interface {
 	Build() *ast.ColumnSpec
 }
 
+type indexBuilder interface {
+	Build() *ast.IndexSpec
+}
+
 type Formatter interface {
 	FormatNode(w io.Writer, n ast.Node)
 }
